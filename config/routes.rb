0e4e707870f682f7fd77877977919921c64b455a
Jose Rodriguez <jmrv002@gmail.com>
2018-11-02 06:50:05 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-
-  get 'redirect_to_previous_page', to: 'pages#previous_page', as: 'previous_page'
-
-  get 'user/:name', to: 'pages#user', as: 'dynamic_link'
-
+  root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
