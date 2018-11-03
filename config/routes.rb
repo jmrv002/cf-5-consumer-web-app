7f78515003ef444cd672ecb4f31628f6f9a01b62
Jose Rodriguez <jmrv002@gmail.com>
2018-11-03 15:20:49 -0400

    add contact form and add functionality to send emails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'simple_pages#landing_page'
+
+  get 'contact', to: 'simple_pages#contact', as: 'contact'
+
+  post 'simple_pages/thank_you'
+  
   resources :orders, only: [:index, :show, :create, :destroy]
+  
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
