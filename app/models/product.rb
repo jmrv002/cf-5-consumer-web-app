4866cb5b131e800fc14f866d944777ccee4084c8
Jose Rodriguez <jmrv002@gmail.com>
2018-11-03 14:05:22 -0400

    5.2 - test search

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,10 @@ class Product < ApplicationRecord
 	has_many :orders
 
   def self.search(search_term)
-    search_term.downcase!
+    # search_term.downcase!
 
     queryString = Rails.env.development? ? 'LIKE' : 'ilike'
 
-    Product.where("name LIKE ?", "%#{search_term}%")
+    Product.where("name #{queryString} ?", "%#{search_term}%")
   end
 end
