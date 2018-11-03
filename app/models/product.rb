1daee4bd81cbf0ec7084da36b530baead3fb0f62
Jose Rodriguez <jmrv002@gmail.com>
2018-11-03 14:06:56 -0400

    5.2 - remove downcast

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
 	has_many :orders
 
   def self.search(search_term)
-    # search_term.downcase!
-
     queryString = Rails.env.development? ? 'LIKE' : 'ilike'
 
     Product.where("name #{queryString} ?", "%#{search_term}%")
