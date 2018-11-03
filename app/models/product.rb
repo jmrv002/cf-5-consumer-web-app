d7ee56bdad38546bac153220142c20c05ed16dd0
Jose Rodriguez <jmrv002@gmail.com>
2018-11-03 14:01:47 -0400

    add downcase convert in search method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 	has_many :orders
 
   def self.search(search_term)
+    search_term.downcase!
+
     queryString = Rails.env.development? ? 'LIKE' : 'ilike'
 
     Product.where("name LIKE ?", "%#{search_term}%")
