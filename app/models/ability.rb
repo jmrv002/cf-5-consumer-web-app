c2912b40515bf7ce16c9e2abe477460866a48e61
Jose Rodriguez <jmrv002@gmail.com>
2018-12-06 00:10:26 -0400

    add admin flag and add the ability to admin users to delete comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.present?
+      can :create, Comment
+      can :manage, User, id: user.id
+
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
 end
